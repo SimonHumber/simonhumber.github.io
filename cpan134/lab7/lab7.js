2fcb7698fd78eb81cefaead30912774132ff5fcd
simon <trns0286@humber.ca>
2023-03-10 17:24:31 -0500

    lab7

---
+++ cpan134/lab7/lab7.js
@@ -0,0 +1,23 @@
+function funcHalloween() {
+    document.getElementById('halloween').style.display = 'block'
+}
+
+function hideHalloween() {
+    document.getElementById('halloween').style.display = 'none'
+}
+
+function funcChristmas() {
+    document.getElementById('christmas').style.display='block'
+}
+
+function hideChristmas() {
+    document.getElementById('christmas').style.display='none'
+}
+
+function funcNewyears() {
+    document.getElementById('newyears').style.display='block'
+}
+
+function hideNewyears() {
+    document.getElementById('newyears').style.display='none'
+}
